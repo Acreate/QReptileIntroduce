5bee1ea547ffd2604dd33d9bf88eef6862f6ba08
Acreate <1552397900@qq.com>
2024-04-06 01:00:47 +0800

    重编译翻译文件

--- srcs/projects/ReptileIntroduce/translations/ReptileIntroduce_zh_CN.ts
+++ srcs/projects/ReptileIntroduce/translations/ReptileIntroduce_zh_CN.ts
@@ -1,3 +1,339 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS/>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Button</name>
+    <message>
+        <location filename="../src/qt/extend/ui/Button.cpp" line="13"/>
+        <source>Button::~Button : </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CountEditWidget</name>
+    <message>
+        <location filename="../src/qt/group/ui/CountEditWidget.cpp" line="13"/>
+        <source>20000</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/CountEditWidget.cpp" line="14"/>
+        <source>加</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/CountEditWidget.cpp" line="15"/>
+        <source>减</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DateTimeThread</name>
+    <message>
+        <location filename="../src/qt/extend/thread/DateTimeThread.cpp" line="15"/>
+        <source>%1 年 %2 月 %3 日 %4:%5:%6</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EditLine</name>
+    <message>
+        <location filename="../src/qt/extend/ui/EditLine.cpp" line="6"/>
+        <source>EditLine::~EditLine : </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="26"/>
+        <source>MainWidget::MainWidget 线程 id : </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="47"/>
+        <source>ini</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="47"/>
+        <source>progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="58"/>
+        <source>加载错误</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="61"/>
+        <source>小说阅读</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="115"/>
+        <source>加载配置文件</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="117"/>
+        <source>requestSettingFilePath, &amp;Action::trigger slots</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="200"/>
+        <source>MainWidget::updateDateTimeStrFunction( const QString &amp;currentDateTimeStr ) currentThreadId: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="216"/>
+        <source>MainWidget::changeTransparent( bool flage ) : </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="244"/>
+        <source>指定一个 web 配置文件</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="244"/>
+        <source>配置类型(*.set *.ini *.setting );;全部类型(*)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="255"/>
+        <source>请选择</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="255"/>
+        <source>文件错误，是否重新选择？</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.cpp" line="269"/>
+        <source>当前 MainWidget 窗口最小的宽度 : </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="68"/>
+        <source>somponentStyle/TransparentForMouseEvents</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="69"/>
+        <source>work/ReadDirPath</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="70"/>
+        <source>work/WriteDirPath</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="71"/>
+        <source>web/SettingFilePath</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="73"/>
+        <source>当前状态: [%1穿透]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/MainWidget.h" line="74"/>
+        <source>未</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>NovelInfoWidget</name>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="49"/>
+        <source>配置文件路径:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="53"/>
+        <source>请输入一个配置文件</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="65"/>
+        <source>错误</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="66"/>
+        <source>
+	Button{
+		color : red;
+	}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="73"/>
+        <source>加载</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="76"/>
+        <source>
+	Button{
+		color : black;
+	}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="84"/>
+        <source>选择一个设置文件</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="86"/>
+        <source>请选择一个选项</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="86"/>
+        <source>文件打开错误!现在重新选择吗?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="97"/>
+        <source>
+				Button{
+					color : blue;
+				}
+				</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="103"/>
+        <source>over locked</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="106"/>
+        <source>
+				Button{
+					color : black;
+				}
+				</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="185"/>
+        <source>NovelInfoWidget::~NovelInfoWidget</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="236"/>
+        <source>====== NovelInfoWidget::showEvent ===========</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="254"/>
+        <source>选择配置文件</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="254"/>
+        <source>配置文件类型(*.ini *.setting *.set);;全部类型(*)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="259"/>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="274"/>
+        <source>请选择</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="259"/>
+        <source>路径无法写入文件。现在重新选择路径吗？</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="268"/>
+        <source>选择一个 web 缓冲目录</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.cpp" line="274"/>
+        <source>路径无法写入。现在重新选择路径吗？</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/NovelInfoWidget.h" line="39"/>
+        <source>work/WebBuff/Path</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Request</name>
+    <message>
+        <location filename="../src/qt/extend/netWork/Request.cpp" line="29"/>
+        <source>homeUrl</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/extend/netWork/Request.cpp" line="35"/>
+        <source>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.289 Safari/537.36 QIHU 360SE</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>WebUrlInfoWidget</name>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.cpp" line="43"/>
+        <source>===========
+文件名: 
+	%1
+调用名称: 
+	WebUrlInfoWidget::WebUrlInfoWidget
+行号: 
+	%2 row
+错误!请为 parent 参数设置一个有效的对象指针
+	parent 总是 NovelInfoWidget* 类型
+===========
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.cpp" line="65"/>
+        <source>文件:
+%1
+行号:
+%2
+信息:
+QSettings * 对象指针不存在 [%3] 的值
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.cpp" line="117"/>
+        <source>WebUrlInfoWidget::~WebUrlInfoWidget : </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.cpp" line="194"/>
+        <source>插入小说网站信息</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.cpp" line="198"/>
+        <source>保存</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.h" line="38"/>
+        <source>host</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/group/ui/WebUrlInfoWidget.h" line="39"/>
+        <source>url</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
