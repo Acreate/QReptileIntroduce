484dfd906cc0c4f0bba702c6e1e9fcba1cdd317a
Acreate <1552397900@qq.com>
2024-12-04 15:11:28 +0800

    增加 www.2wxsl.com 网址爬虫

---
+++ srcs/libs/ShaderModel/plugs/www.2wxsl.com/RequestNetPlugin/RequestNetPlugin.cpp
@@ -0,0 +1,11 @@
+﻿#include "RequestNetPlugin.h"
+
+#include "../RequestNet/RequestNet.h"
+RequestNetPlugin::RequestNetPlugin( QObject *parent ): QGenericPlugin( parent ) {
+}
+RequestNetPlugin::~RequestNetPlugin( ) {
+}
+
+QObject *RequestNetPlugin::create( const QString &name, const QString &spec ) {
+	return new RequestNet( this );
+}
---
+++ srcs/libs/ShaderModel/plugs/www.2wxsl.com/auto_generate_files/macro/cmake_to_c_cpp_header_macro.h
@@ -0,0 +1,8 @@
+﻿#ifndef WWW_2WXSL_COM_HEADER_MACRO_H_H_HEAD__FILE__
+#define WWW_2WXSL_COM_HEADER_MACRO_H_H_HEAD__FILE__
+#pragma once
+
+#define IRequestNetInterface_iid	"www.2wxsl.com.json.IRequestNetInterfaceExtend"
+#define GET_URL	u8"http://www.2wxsl.com"
+
+#endif // WWW_2WXSL_COM_HEADER_MACRO_H_H_HEAD__FILE__
