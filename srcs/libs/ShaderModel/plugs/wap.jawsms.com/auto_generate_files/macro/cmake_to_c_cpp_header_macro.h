958db9b6144d02a422e1dc1bf88a42febdae69ca
Acreate <1552397900@qq.com>
2025-07-24 21:22:39 +0800

    增加 wap.jawsms.com

---
+++ srcs/libs/ShaderModel/plugs/wap.jawsms.com/auto_generate_files/macro/cmake_to_c_cpp_header_macro.h
@@ -0,0 +1,8 @@
+﻿#ifndef WAP_JAWSMS_COM_HEADER_MACRO_H_H_HEAD__FILE__
+#define WAP_JAWSMS_COM_HEADER_MACRO_H_H_HEAD__FILE__
+#pragma once
+
+#define IRequestNetInterface_iid	"wap.jawsms.com.json.IRequestNetInterfaceExtend"
+#define GET_URL	u8"http://wap.jawsms.com"
+
+#endif // WAP_JAWSMS_COM_HEADER_MACRO_H_H_HEAD__FILE__
